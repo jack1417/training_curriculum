2a839a8d5c5a60d183f8e3e4420a9c542cc3545c
jack1417 <78401554+jack1417@users.noreply.github.com>
2021-02-12 19:29:11 +0900

    やり直し

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:plan).permit(:date, :plan)
+    params.require(:contloier).permit(:date, :plan)
   end
 
   def get_week
@@ -35,11 +35,8 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = Date.today.wday + x 
-      if wday_num >= 7                        
-        wday_num = wday_num -7
-      end            
-      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
+                
+      days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, }
       @week_days.push(days)
     end
 
